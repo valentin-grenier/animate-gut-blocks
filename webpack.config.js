edaf70de324bdbad632ab695d876e35ac3dfb7bc
Valentin Grenier <valentin.grenier02@gmail.com>
2026-01-27 09:59:57 +0100

    refactor: Simplify webpack configuration by extending default WordPress scripts and removing custom output and module rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,15 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+	...defaultConfig,
 	entry: {
 		editor: './src/js/editor.js',
 		frontend: './src/js/frontend.js',
 	},
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: '[name].js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-					},
-				},
-			},
-			{
-				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-		}),
-	],
-	externals: {
-		'@wordpress/i18n': 'wp.i18n',
-		'@wordpress/hooks': 'wp.hooks',
-		'@wordpress/compose': 'wp.compose',
-		'@wordpress/element': 'wp.element',
-		'@wordpress/block-editor': 'wp.blockEditor',
-		'@wordpress/components': 'wp.components',
-	},
+	plugins: defaultConfig.plugins.filter(
+		(plugin) => plugin.constructor.name !== 'RtlCssPlugin'
+	),
+	devtool: isProduction ? false : 'source-map',
 };
